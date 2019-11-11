2ae275125415ecdf7e9a7071d95c9caf358a4c91
JCeSN <31513121+JCeSN@users.noreply.github.com>
2019-11-11 15:43:12 +0100

    Update climat.R

--- climat.R
+++ climat.R
@@ -1,25 +1,12 @@
 library(ncdf4)
 
 
-setwd("C:/Users/julio/Documents/R/New_beginning//Datos_projet")
+setwd("sss")
 
 ncin =  nc_open("data_GFDL_CM3.cdf")
-print(ncin)
 lon = ncvar_get(ncin, "lon")
 lat = ncvar_get(ncin, "lat")
 idex.array = ncvar_get(ncin, "spei-all")
-dim(lon)
-dim(lat)
-dim(idex.array)
 slice = idex.array[,,1]
 
 image(lon, lat, slice)
-
-# unidades= ncatt_get(ncin, "spei-all", "units")
-# unitlon = ncatt_get(ncin, "lon", "units")
-# unit = ncatt_get(ncin, "T", "units")
-# unit
-# slice = idex.array(,,1)
-# image(lon, lat, slice)
-
-
